291ebf97a3d46882410f7497b61abd8806cac495
wwvl <github-action@actions-template-sync.noreply.github.com>
2024-10-18 16:02:39 +0000

    chore(template): merge template changes :up:
    Signed-off-by: wwvl <github-action@actions-template-sync.noreply.github.com>

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "wwvl/Emma:main"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
